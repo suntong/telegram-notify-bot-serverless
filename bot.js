ad66a6181b4de493c18e5c72db88ccfa427ceb4e
Tong Sun <suntong@cpan.org>
2023-01-24 18:55:00 -0500

    - [+] add sendLongMsg()

--- bot.js
+++ bot.js
@@ -13,11 +13,25 @@ bot.start((ctx) => {
     return ctx.reply(`Hello from Lambda, ${ctx.from.first_name ? ctx.from.first_name : 'friend'}! Use /help to view available commands.`);
 });
 
+// chunk util
+const chunk = (str, size) =>
+  Array.from({ length: Math.ceil(str.length / size) }, (v, i) =>
+    str.slice(i * size, i * size + size)
+  )
+;
+
+async function sendLongMsg(reply) {
+    const longMsg = '.'.repeat(4096 *3)
+    for (const part of chunk(longMsg, 4096))
+		await reply(part)
+}
+
 bot.help(async (ctx) => {
-    for(let i =1; i <= 3; i++) await ctx.reply(`Reply - ${i}`)
-    for(let i =1; i <= 5; i++) await ctx.replyWithMarkdown(`Reply - *${i}*`)
+    //for(let i =1; i <= 3; i++) await ctx.reply(`Reply - ${i}`)
+    //for(let i =1; i <= 5; i++) await ctx.replyWithMarkdown(`Reply - *${i}*`)
     const s = '.'.repeat(4096)
-    for(let i =1; i <= 3; i++) await ctx.replyWithMarkdown(s)
+    //for(let i =1; i <= 3; i++) await ctx.replyWithMarkdown(s)
+    await sendLongMsg(ctx.reply)
     return ctx.replyWithMarkdown(helpMsg);
 });
 
